a43760ae42ccc246866dd593f724d19df2dd69ee
Jinyoung Jang <jyjang@uengine.org>
2023-02-21 13:09:57 +0900

    commit

---
+++ indexer/src/main/java/untitled/domain/FileIndexed.java
@@ -0,0 +1,22 @@
+package untitled.domain;
+
+import java.util.*;
+import lombok.*;
+import untitled.domain.*;
+import untitled.infra.AbstractEvent;
+
+@Data
+@ToString
+public class FileIndexed extends AbstractEvent {
+
+    private Long id;
+    private Long fileId;
+
+    public FileIndexed(Index aggregate) {
+        super(aggregate);
+    }
+
+    public FileIndexed() {
+        super();
+    }
+}
---
+++ video processing/src/main/java/untitled/domain/VideoProcessed.java
@@ -0,0 +1,23 @@
+package untitled.domain;
+
+import java.util.*;
+import lombok.*;
+import untitled.domain.*;
+import untitled.infra.AbstractEvent;
+
+@Data
+@ToString
+public class VideoProcessed extends AbstractEvent {
+
+    private Long id;
+    private String url;
+    private String fileKey;
+
+    public VideoProcessed(Video aggregate) {
+        super(aggregate);
+    }
+
+    public VideoProcessed() {
+        super();
+    }
+}
